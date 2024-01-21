6d1312278bcc06065a8555f86a4aa8ba166b48de
Sam Matenaer <smatenaer@esri.com>
2024-01-21 17:59:33 -0600

    Revert "analytics"
    This reverts commit cda4cee110b461b6196fce5b9ea467d5561ca6fd.

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,13 @@
 import './index.css';
+import 'tippy.js/dist/tippy.css';
 
-import { Analytics } from '@vercel/analytics/react';
 import LogRocket from 'logrocket';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
-
 LogRocket.init('fk6gsk/big-brain-town');
 
 const root = createRoot(document.querySelector('#root')!);
 
-root.render(
-  <>
-    <App />
-    <Analytics />
-  </>,
-);
+root.render(<App />);
